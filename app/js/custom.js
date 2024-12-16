8c8bdd79164d142d5709ef3b59f14f2e24bf1acf
Elastel <elastel.group@gmail.com>
2024-12-16 07:05:13 +0000

    Wireguard: Add the configuration of local private key , multiple clients
    Signed-off-by: Elastel <elastel.group@gmail.com>

--- app/js/custom.js
+++ app/js/custom.js
@@ -922,6 +922,7 @@ $('.wg-keygen').click(function(){
     $.post('ajax/networking/get_wgkey.php',{'entity':entity_pub.attr('name') },function(data){
         var jsonData = JSON.parse(data);
         entity_pub.val(jsonData.pubkey);
+        $('#wg-srvprikey').val(jsonData.privkey);
         $('#' + updated).removeClass('check-hidden').addClass('check-updated').delay(500).animate({ opacity: 1 }, 700);
     })
 })
