a26e5fe9854108127e377f17de9b3af736e8f1fb
Elastel <elastel.group@gmail.com>
2025-10-30 07:22:40 +0000

    v1.0.55 Optimizing Data Collection Rules to Resolve Conflicts with Identical Tag Names
    Signed-off-by: Elastel <elastel.group@gmail.com>

--- app/js/dct.js
+++ app/js/dct.js
@@ -16,6 +16,10 @@ function doesColumnExist(tableId, columnName) {
 function writeValueByTag(object) {
     var tds = $(object).parent().parent().find("td");
     var tagName = tds.filter('[name="factor_name"]').text();
+    var serverCenter = tds.filter('[name="server_center"]').text();
+    if (serverCenter == '' || serverCenter == '-') {
+        serverCenter = '0';
+    }
 
     const overlay = document.createElement('div');
     overlay.style.position = 'fixed';
@@ -114,11 +118,12 @@ function writeValueByTag(object) {
         let params = '';
         if (tagName.includes(';')) {
             const selectedValue = labelOrSelect.value;
-            params = 'tagName=' + selectedValue + '&' + 'value=' + input.value;
+            params = 'tagName=' + selectedValue + ';' + serverCenter + '&' + 'value=' + input.value;
         } else {
-            params = 'tagName=' + tagName + '&' + 'value=' + input.value
+            params = 'tagName=' + tagName + ';' + serverCenter + '&' + 'value=' + input.value
         }
         
+        console.log(params);
         if (input.value.length > 0) {
             $.get('ajax/dct/get_dctcfg.php?type=tag_write&' + params, function(data) {}); 
         } else {
@@ -727,6 +732,15 @@ function getRealtimeData() {
         if (jsonData == null)
             return false;
 
+        const jsonResult = Object.entries(jsonData).map(([key, value]) => {
+            if (key.includes(";"))  {
+                const [name, index] = key.split(";");
+                return [name, index, value];
+            } else {
+                return [key, 0, value];
+            }
+        });
+
         const trList = document.querySelectorAll('table tr');
         var dnp3 = document.getElementById('option_list_dnp3');
         var modbus_slave = document.getElementById('option_list_modbus_slave_point');
@@ -736,20 +750,28 @@ function getRealtimeData() {
                 if (tr.querySelector('td[name="source_object"]')) {
                     var factor = tr.querySelector('td[name="source_object"]').innerHTML;
                     factor = factor.substring(factor.indexOf('-') + 1)
-                    if (jsonData.hasOwnProperty(factor)) {
-                        cur_value += jsonData[factor];
+                    const jsonItem = jsonResult.find(([name]) => name === factor);
+                    if (jsonItem) {
+                        cur_value += jsonItem[2];
                     }
                 }
             } else {
                 if (tr.querySelector('td[name="factor_name"]')) {
                     //console.log(tr.querySelector('td[name="factor_name"]').innerHTML);
                     var factor = tr.querySelector('td[name="factor_name"]').innerHTML;
+                    var serverCenter = tr.querySelector('td[name="server_center"]').innerHTML;
                     var factorList = factor.split(';');
                     factorList.forEach((key) => {
                         // console.log(key);
-                        if (jsonData.hasOwnProperty(key)) {
-                            cur_value += jsonData[key] + ';';
-                        }    
+                        var jsonValue = '';
+                        jsonResult.forEach(item => {
+                            const [name, index, value] = item;
+                            if (name == key && ((1 << (serverCenter -1)) == parseInt(index) || index == 0)) {
+                                jsonValue = value;
+                                return;
+                            }
+                        });
+                        cur_value += jsonValue + ';'; 
                     })
 
                     if (cur_value.slice(-1) === ';') {
@@ -2111,48 +2133,46 @@ function loadModbusSlaveConfig() {
     });
 }
 
-function addDataDisplyItem(tbody, table, jsonData, key, keywords) {
-    if (jsonData.hasOwnProperty(key)) {
-        if (!key.includes(keywords) && keywords.length > 0) {
-            return false;
-        }
+function addDataDisplyItem(tbody, table, key, value, keywords) {
+    if (!key.includes(keywords) && keywords.length > 0) {
+        return false;
+    }
 
-        var td = table.querySelector('td[name="' + key + '"]');
-        if (td) {
-            var tr = td.parentNode;
-            tr.children[1].textContent = jsonData[key];
-        } else {
-            var tr = document.createElement('tr');
-            tr.className = "tr cbi-section-table-descr";
+    var td = table.querySelector('td[name="' + key + '"]');
+    if (td) {
+        var tr = td.parentNode;
+        tr.children[1].textContent = value;
+    } else {
+        var tr = document.createElement('tr');
+        tr.className = "tr cbi-section-table-descr";
 
-            // key
-            var tdKey = document.createElement('td');
-            tdKey.style.textAlign = "center";
-            tdKey.setAttribute('name', "factor_name");
-            tdKey.textContent = key;
-            tr.appendChild(tdKey);
+        // key
+        var tdKey = document.createElement('td');
+        tdKey.style.textAlign = "center";
+        tdKey.setAttribute('name', "factor_name");
+        tdKey.textContent = key;
+        tr.appendChild(tdKey);
 
-            // value
-            var tdValue = document.createElement('td');
-            tdValue.style.textAlign = "center";
-            tdValue.textContent = jsonData[key];
-            tr.appendChild(tdValue);
+        // value
+        var tdValue = document.createElement('td');
+        tdValue.style.textAlign = "center";
+        tdValue.textContent = value;
+        tr.appendChild(tdValue);
 
-            // button
-            var tdBtn = document.createElement('td');
-            tdBtn.style.textAlign = "center";
-            let button = document.createElement("button");
-            button.textContent = "Write";
-            button.classList.add("btn-primary");
-            button.style = "border-radius: 0.5rem;";
-            button.addEventListener("click", function (event) {
-                event.preventDefault();
-                writeValueByTag(this);
-            });
-            tdBtn.appendChild(button);
-            tr.appendChild(tdBtn);
-            tbody.appendChild(tr);
-        }
+        // button
+        var tdBtn = document.createElement('td');
+        tdBtn.style.textAlign = "center";
+        let button = document.createElement("button");
+        button.textContent = "Write";
+        button.classList.add("btn-primary");
+        button.style = "border-radius: 0.5rem;";
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            writeValueByTag(this);
+        });
+        tdBtn.appendChild(button);
+        tr.appendChild(tdBtn);
+        tbody.appendChild(tr);
     }
 }
 
@@ -2173,6 +2193,16 @@ function getWebshowDate() {
             return;
         }
 
+        const jsonResult = Object.entries(jsonData).map(([key, value]) => {
+            if (key.includes(";"))  {
+                const [name, index] = key.split(";");
+                return [name, index, value];
+            } else {
+                return [key, 0, value];
+            }
+        });
+
+
         var table = document.getElementsByTagName("table")[0];
         var keywords = document.getElementsByName("keywords")[0].value;
         var select = document.getElementById('current_rule').value || "all";
@@ -2181,9 +2211,12 @@ function getWebshowDate() {
             var td = tr.querySelector('td[name]');
             if (td) {
                 var key = td.getAttribute('name');
-                if (!jsonData.hasOwnProperty(key) || (keywords.length > 0 && !key.includes(keywords))) {
-                    tr.remove();
-                }
+                jsonResult.forEach(item => {
+                    if (key = item[0] || (keywords.length > 0 && !key.includes(keywords))) {
+                        tr.remove();
+                        return;
+                    }
+                });
             }
         });
 
@@ -2193,11 +2226,10 @@ function getWebshowDate() {
         }
         var tbody = table.tBodies[0];
         if (select == "all") {
-            for (var key in jsonData) {
-                if (!addDataDisplyItem(tbody, table, jsonData, key, keywords)) {
-                    continue;
-                }
-            }
+            jsonResult.forEach(item => {
+                const [name, index, value] = item;
+                addDataDisplyItem(tbody, table, name, value, keywords);
+            });
         } else {
             factorList.forEach(function(item) {
                 if (!item.startsWith(select + '-')) {
@@ -2205,8 +2237,10 @@ function getWebshowDate() {
                 }
 
                 var key = item.substring(item.indexOf('-') + 1);
-                if (!addDataDisplyItem(tbody, table, jsonData, key, keywords)) {
-                    return;
+                const jsonItem = jsonResult.find(([name]) => name === key);
+                if (jsonItem) {
+                    const value = jsonItem[2];
+                    addDataDisplyItem(tbody, table, key, value, keywords);
                 }
             });
         }
