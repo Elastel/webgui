6bdef82a365c74e7516d7fff40dad35f7bcaa9dd
elastel <elastel@example.com>
2025-12-29 09:10:30 +0000

    webgui: Optimize online update commands
    Signed-off-by: elastel <elastel@example.com>

--- ajax/system/system.php
+++ ajax/system/system.php
@@ -46,7 +46,7 @@ if ($type == "node_online_update") {
         $data['error'] = 'No network!';
     }
 } else if ($type == "reset_configs") {
-    exec('cd /var/www/html; sudo git checkout *');
+    exec('cd /var/www/html; sudo git reset --hard HEAD');
     exec('sudo /var/www/html/update reset 2>&1');
 } else if ($type == "download_backup") {
     exec('sudo rm -f /tmp/backup.tar.gz');
